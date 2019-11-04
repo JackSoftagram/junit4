189a06dd6191140df0aed42039bc8e50982e150e
Jack the Tester of Softagram <50484747+JackSoftagram@users.noreply.github.com>
2019-11-04 13:09:24 +0200

    Update Assert.java

--- src/main/java/org/junit/Assert.java
+++ src/main/java/org/junit/Assert.java
@@ -1,11 +1,8 @@
 package org.junit;
-//test
-//test
-//test
 import org.hamcrest.Matcher;
 import org.hamcrest.MatcherAssert;
 import org.junit.validator.TestClassValidator; // Dependency rules
-import org.junit.function.ThrowingRunnable;
+//import org.junit.function.ThrowingRunnable;
 import org.junit.internal.ArrayComparisonFailure;
 import org.junit.internal.ExactComparisonCriteria;
 import org.junit.internal.InexactComparisonCriteria;
