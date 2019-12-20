63efe7c285b9e79ba07908d800ff5af463d1bbe7
Ville Laitila <ville.laitila@softagram.com>
2019-12-20 15:46:41 +0200

    Update Throwables.java

--- src/main/java/org/junit/internal/Throwables.java
+++ src/main/java/org/junit/internal/Throwables.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import junit.runner.Version; // shouldn't do this really! just a demo
+import junit.runner.Version; // shouldn't do this really! just a demo..
 import junit.tests.SampleJUnit3Tests; //for report testing
 
 
